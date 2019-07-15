e6ca42979c7e085fe0f612796a34a3f34dd17a16
sportsradical <peereira@live.com.pt>
2019-07-15 16:49:45 +0100

    Add files via upload

---
+++ Tutorial 41 Multiple Light Shadow Mapping/Engine/Engine/source/lightclass.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: lightclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _LIGHTCLASS_H_
+#define _LIGHTCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: LightClass
+////////////////////////////////////////////////////////////////////////////////
+class LightClass
+{
+public:
+	LightClass();
+	LightClass(const LightClass&);
+	~LightClass();
+
+	void SetAmbientColor(float, float, float, float);
+	void SetDiffuseColor(float, float, float, float);
+	void SetPosition(float, float, float);
+	void SetLookAt(float, float, float);
+
+	XMFLOAT4 GetAmbientColor();
+	XMFLOAT4 GetDiffuseColor();
+	XMFLOAT3 GetPosition();
+
+	void GenerateViewMatrix();
+	void GenerateProjectionMatrix(float, float);
+
+	void GetViewMatrix(XMMATRIX&);
+	void GetProjectionMatrix(XMMATRIX&);
+
+private:
+
+	XMFLOAT4 m_ambientColor;
+	XMFLOAT4 m_diffuseColor;
+	XMFLOAT3 m_position;
+	XMFLOAT3 m_lookAt;
+	XMMATRIX m_viewMatrix;
+	XMMATRIX m_projectionMatrix;
+
+
+};
+
+#endif
